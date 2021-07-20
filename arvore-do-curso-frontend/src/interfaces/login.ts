eac4db3e3370bf6d4bc79437353993562f70c0fb
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-07-20 05:25:54 -0400

    Setup Finalizado. Esqueleto da página inicial finalizado.

---
+++ arvore-do-curso-frontend/src/interfaces/login.ts
@@ -0,0 +1,4 @@
+export default interface Login {
+    usuario: string
+    senha: string
+}
---
+++ arvore-do-curso-frontend/src/pages/index.tsx
@@ -0,0 +1,57 @@
+import Head from "next/head"
+import Login from "../interfaces/login"
+import { useForm, SubmitHandler } from "react-hook-form"
+import styles from "../styles/pages/Home.module.css"
+
+export default function Home() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<Login>()
+    const onSubmit: SubmitHandler<Login> = (data) => console.log(data)
+
+    return (
+        <div>
+            <Head>
+                <title>Página de Login</title>
+            </Head>
+
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>
+                            Email
+                            <input
+                                type="email"
+                                {...register("usuario", {
+                                    required: true,
+                                    pattern:
+                                        /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i,
+                                })}
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <label>
+                            Senha
+                            <input
+                                type="password"
+                                {...register("senha", { required: true })}
+                            />
+                        </label>
+                    </div>
+
+                    <div>
+                        <button type="submit">Login</button>
+                    </div>
+                </form>
+                <p>Não possui cadastro?</p>
+                <p>
+                    <span>Cadastre-se aqui</span>
+                </p>
+            </div>
+        </div>
+    )
+}
