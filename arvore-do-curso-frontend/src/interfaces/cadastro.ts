0ed1eb3b22797f77f089030e4f9ea59b1913f28a
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-07-21 04:42:20 -0400

    Esqueleto da página de cadastro de usuário quase finalizado.

---
+++ arvore-do-curso-frontend/src/interfaces/cadastro.ts
@@ -0,0 +1,6 @@
+export interface CadastroForm{
+    nome: string;
+    email: string
+    senha: string
+    confirmSenha: string
+}
--- arvore-do-curso-frontend/src/pages/cadastro/index.tsx
+++ arvore-do-curso-frontend/src/pages/cadastro/index.tsx
@@ -1,5 +1,31 @@
+import Head from "next/head"
+import Link from "next/link"
+import {CadastroForm} from "../../interfaces/cadastro"
+import { useForm, SubmitHandler } from "react-hook-form"
+
+
 export default function Cadastro() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<CadastroForm>()
+    const onSubmit: SubmitHandler<CadastroForm> = (data) => console.log(data)
     return (
-        <h1>Tela de Cadastro</h1>
+        <div>
+            <Head>
+                <title>Cadastro de Usuário</title>
+            </Head>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label>
+                            <input {...register("nome", {required: true, pattern: /[\w]+/})} 
+                            placeholder="Digite seu nome..."/>
+                        </label>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
